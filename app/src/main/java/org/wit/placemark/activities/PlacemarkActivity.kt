befce026404d845507d9c062d03f8edf8de4ee27
Mathana Nair Sreedaran <mathana96@gmail.com>
2018-09-17 07:05:36 +0100

    [Lab 1] Complete
    Signed-off-by: Mathana Nair Sreedaran <mathana96@gmail.com>

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,35 @@
+package org.wit.placemark.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_placemark.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.models.PlacemarkModel
+
+
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+
+  var placemark = PlacemarkModel()
+  val placemarks = ArrayList<PlacemarkModel>()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placemark)
+    info("Placemark Activity started..")
+
+    btnAdd.setOnClickListener() {
+      placemark.title = placemarkTitle.text.toString()
+      if (placemark.title.isNotEmpty()) {
+        placemarks.add(placemark)
+        info("add Button Pressed: $placemarkTitle")
+        info("This is the placemarks arraylist: $placemarks")
+      }
+      else {
+        toast ("Please Enter a title")
+      }
+    }
+  }
+}
