f80bcdd5be682d36a02c427b41ab662ece0f1f6f
Mathana Nair Sreedaran <mathana96@gmail.com>
2018-09-25 20:10:18 +0100

    [Lab 2] Move PlacemarkAdapter to its own file
    Signed-off-by: Mathana Nair Sreedaran <mathana96@gmail.com>

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -3,14 +3,13 @@ package org.wit.placemark.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.*
 import kotlinx.android.synthetic.main.activity_placemark_list.*
-import kotlinx.android.synthetic.main.card_placemark.view.*
 import org.jetbrains.anko.startActivityForResult
 import org.wit.placemark.R
+import org.wit.placemark.adapters.PlacemarkAdapter
 import org.wit.placemark.main.MainApp
-import org.wit.placemark.models.PlacemarkModel
+
 
 
 class PlacemarkListActivity : AppCompatActivity() {
@@ -41,25 +40,3 @@ class PlacemarkListActivity : AppCompatActivity() {
   }
 }
 
-class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) : RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-    return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_placemark, parent, false))
-  }
-
-  override fun onBindViewHolder(holder: MainHolder, position: Int) {
-    val placemark = placemarks[holder.adapterPosition]
-    holder.bind(placemark)
-  }
-
-  override fun getItemCount(): Int = placemarks.size
-
-  class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    fun bind(placemark: PlacemarkModel) {
-      itemView.placemarkTitle.text = placemark.title
-      itemView.description.text = placemark.description
-    }
-
-
-  }
-}
